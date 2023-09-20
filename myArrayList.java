f686617e753f2757e79fc8927639328e06ff8230
juliakratzer <juliakratzer@localhost>
2023-09-20 10:46:05 -0400

    Remove method

--- myArrayList.java
+++ myArrayList.java
@@ -1,1 +1,8 @@
 
+public boolean Fraction remove (Fraction input); {
+	for (int i = 0; i < size; i++) {
+        if (elements[i].hasSameValue(input)) {
+            Fraction removed = elements[i];
+}
+	}
+}}
